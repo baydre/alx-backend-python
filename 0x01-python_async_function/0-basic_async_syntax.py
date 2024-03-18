cb307e49fa0587a6dde006896e5255c2ec09dc99
baydre_africa <baydreafrica@gmail.com>
2024-03-18 23:22:54 +0100

    asynchronous coroutine that takes in an integer agrument

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,12 +8,5 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
+
     return delay
-
-
-async def main():
-    random_delay = await wait_random()
-    print(f"Random delay: {random_delay:.2f}seconds")
-
-if __name__ == "__main__":
-    asyncio.run(main())
