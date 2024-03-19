97a8df03e005396b6ef3dc61e9c8ea1e1375007e
baydre_africa <baydreafrica@gmail.com>
2024-03-19 17:08:57 +0100

    async_generator coroutine that takes no arguments

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task_0: async_generator coroutine
+that takes no arguments.
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """
+    loops 10 times asynchronously, wait 1 seconds
+    and then yields a random number between 0 - 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
