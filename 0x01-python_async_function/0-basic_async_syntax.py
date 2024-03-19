194c09451502488f13af31a078c77e31dea40193
baydre_africa <baydreafrica@gmail.com>
2024-03-19 09:34:39 +0100

    fix: asynchronous coroutine that takes in an integer argument

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+"""
+Task_0: async basics, coroutine
+that takes in an integer argument.
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
+    waits for random delay
     """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
 
     return delay
