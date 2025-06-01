8c4764a150e30f22db78dc419fb78b923391d0ac
baydre_africa <baydreafrica@gmail.com>
2025-06-01 22:10:37 +0100

    feat: implement API endpoints for conversations and messages chore: modified settings file to use Custom User model

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        sender = self.request.user if self.request.user.is_authenticated else None
+        serializer.save(sender=sender)
