affcd2afa3b89692948cbb0646adb45c37c4152e
baydre_africa <baydreafrica@gmail.com>
2025-06-02 11:56:48 +0100

    chore: fix error checks for viewsets listing conversation & messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework import filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -8,6 +9,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
 
     def perform_create(self, serializer):
         serializer.save()
@@ -16,6 +18,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
 
     def perform_create(self, serializer):
         sender = self.request.user if self.request.user.is_authenticated else None
