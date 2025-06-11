3c7063e33d4693996432ce0c8cf96793cc135912
baydre_africa <baydreafrica@gmail.com>
2025-06-11 09:42:20 +0100

    feat: implement authentication to enable users to access their own messages and conversations

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -133,6 +133,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
