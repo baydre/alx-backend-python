34baa8478fe3835b57d40d2a7b3460a6f6e7ff2c
baydre_africa <baydreafrica@gmail.com>
2025-06-18 16:19:46 +0100

    feat: Custom ORM Manager for Unread Messages

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -55,9 +55,7 @@ def user_sent_messages(request):
     )
     return render(request, 'messaging/user_sent_messages.html', {'messages': messages})
 
-from .models import Message
-
 @login_required
 def unread_inbox(request):
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user).only('id', 'sender', 'content', 'timestamp')
     return render(request, 'messaging/unread_inbox.html', {'unread_messages': unread_messages})
